7e608a1c43ecdb2203244c19b8170f47249fd8e9
CarlosCusi1 <ccusi@cipsa.com.pe>
2026-03-29 02:48:43 -0500

    fix: G360 full skill - logo, colors, signature format G360|CCUSI

--- src/components/G360Signature.tsx
+++ src/components/G360Signature.tsx
@@ -1,9 +1,9 @@
-export const G360Signature = ({ cliente = 'CIPSA', showVersion = true, version = '3.0' }) => {
+export const G360Signature = ({ cliente = 'CCUSI', showVersion = true, version = '3.0' }) => {
   return (
     <div className="g360-signature">
-      <span className="g360-signature-brand">{cliente}</span>
-      <span className="g360-signature-text">powered by</span>
-      <span className="g360-signature-author">G360</span>
+      <span className="g360-signature-brand">G360</span>
+      <span className="g360-signature-divider">|</span>
+      <span className="g360-signature-author">{cliente}</span>
       {showVersion && (
         <>
           <span className="g360-signature-separator">•</span>
