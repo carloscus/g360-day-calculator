eab1c89404ee75222a3081807504ec11398a62f8
CarlosCusi1 <ccusi@cipsa.com.pe>
2026-04-29 22:36:48 -0500

    fix: despliegue funcional con rutas absolutas y archivos estáticos en public

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: './',
+  base: '/g360-day-calculator/',
   server: {
     port: 3000,
     open: true,
